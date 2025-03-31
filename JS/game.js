f12ba56903a75c446ad462126b7db93c33ba3c1c
zanm3 <zan.markuza@tscng.org>
2025-03-31 10:30:11 +0200

    update

--- JS/game.js
+++ JS/game.js
@@ -5,18 +5,48 @@ function drawIt() {
     let dy = 4;
     let ctx;
     let canvas;
-    function init() {
+    let rightDown = false;
+    let leftDown = false;
 
+    //nastavljanje leve in desne tipke
+    function onKeyDown(evt) {
+        if (evt.keyCode == 39)
+            rightDown = true;
+        else if (evt.keyCode == 37) 
+            leftDown = true;
+    }
+
+    function onKeyUp(evt) {
+        if (evt.keyCode == 39)
+            rightDown = false;
+        else if (evt.keyCode == 37) 
+            leftDown = false;
+    }
+
+    function init() {
         canvas = document.getElementById('canvas');
         ctx = canvas.getContext('2d');
         return setInterval(draw, 10); //klic funkcije draw vsakih 10 ms; http://www.w3schools.com/jsref/met_win_setinterval.asp
     }
+    function init_paddle() {
+        paddlex = canvas.width / 2;
+        paddleh = 10;
+        paddlew = 75;
+    }
     function draw() {
-        ctx.clearRect(0, 0, 300, 300);
+        // risanje
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+        init_paddle();
+        ctx.rect(paddlex, canvas.height - paddleh, paddlew, paddleh);
         ctx.closePath();
         ctx.fill();
+        
+        if (rightDown) 
+            paddlex += 5;
+        else if (leftDown) 
+            paddlex -= 5;
 
         if (x + dx > canvas.width - 10 || x + dx < 0 + 10)
             dx = -dx;
@@ -25,11 +55,6 @@ function drawIt() {
 
         x += dx;
         y += dy;
-
-        let paddlex;
-        let paddleh;
-        let paddlew;
-        ctx.rect(paddlex, canvas.height - paddleh, paddlew, paddleh);
     }
     init();
 }
